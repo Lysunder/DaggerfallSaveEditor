a828da3f6922cda1efabbee50dc01b57fca8122d
Lysunder <17260770+Lysunder@users.noreply.github.com>
2025-10-01 23:43:42 -0400

    Material UI and initial tabs

--- daggerfall-save-editor/src/app.tsx
+++ daggerfall-save-editor/src/app.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Button from "@mui/material/Button";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import CharacterTabs from "./components/characterTabs.component";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <CharacterTabs />
+  </ThemeProvider>
+);
--- daggerfall-save-editor/src/renderer.ts
+++ daggerfall-save-editor/src/renderer.ts
@@ -25,9 +25,6 @@
  *  });
  * ```
  */
-
-import './index.css';
-
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack',
 );
