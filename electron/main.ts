9a91cd16cc8e2837ab8b3dc0a7d7ebdc5fcdcde7
Lysunder <17260770+Lysunder@users.noreply.github.com>
2026-09-09 20:24:41 -0400

    title and menu updates

--- electron/main.ts
+++ electron/main.ts
@@ -30,6 +30,8 @@ function createWindow() {
     },
   });
 
+  win.setMenu(null);
+
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', (new Date).toLocaleString());
